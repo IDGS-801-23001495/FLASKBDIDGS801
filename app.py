49b153976fe498bf913ddb562efce83125627aff
IDGS-801-23001495 <sralanflores199@gmail.com>
2026-02-16 19:04:39 -0600

    insertar 16/02/2026

--- app.py
+++ app.py
@@ -14,7 +14,10 @@ csrf=CSRFProtect()
 @app.route("/")
 @app.route("/index")
 def index():
-	return render_template("index.html")
+	create_from=forms.UserForm2(request.form)
+	#ORM select * from alumnos
+	alumno=Alumnos.query.all()
+	return render_template("index.html", form=create_from,alumno=alumno)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -22,7 +25,19 @@ def page_not_found(e):
 
 @app.route("/alumnos", methods=["GET","POST"])
 def alumnos():
+	create_form=forms.UserForm2(request.form)
+	if request.method=='POST':
+		alum=Alumnos(nombre=create_form.nombre.data,
+			     apaterno=create_form.apaterno.data,
+				 email=create_form.correo.data)
+		db.session.add(alum)
+		db.session.commit()
+		return redirect(url_for('index'))
 	return render_template("Alumnos.html")
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	csrf.init_app(app)
+	db.init_app(app)
+	with app.app_context():
+		db.create_all()
+	app.run()
