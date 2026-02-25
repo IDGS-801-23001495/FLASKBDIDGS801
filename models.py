bb04784386fdca248f9620d458903e36ba791f90
IDGS-801-23001495 <sralanflores199@gmail.com>
2026-02-25 17:57:57 -0600

    ABC Completo

--- models.py
+++ models.py
@@ -5,8 +5,11 @@ import datetime
 db = SQLAlchemy()
 class Alumnos(db.Model):
     _tablename_='alumnos'
-    id=db.Column(db.Integer, primary_key=True)
-    nombre=db.Column(db.String(50))
-    apaterno=db.Column(db.String(50))
-    email=db.Column(db.String(50))
-    create_date=db.Column(db.DateTime, default=datetime.datetime.now)
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50))
+    apellidos = db.Column(db.String(200))
+    email = db.Column(db.String(120))
+    telefono = db.Column(db.String(20))
+    create_date = db.Column(db.DateTime, 
+                            default=datetime.datetime.now
+                            )
